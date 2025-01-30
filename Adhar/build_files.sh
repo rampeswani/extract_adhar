c8954eb6c0c914deae1568d50815d43e5d23c225
rampeswani <peswani.ram.33@gmail.com>
2025-01-30 12:48:23 +0530

    new 2

--- Adhar/build_files.sh
+++ Adhar/build_files.sh
@@ -1,14 +1,10 @@
 echo "BUILD START"
 
-# Update package manager and install PostgreSQL development libraries
-apt-get update && apt-get install -y libpq-dev gcc
+# Install dependencies
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 
-# Ensure pip and dependencies are installed
-python3.12 -m ensurepip --upgrade
-python3.12 -m pip install --upgrade pip
-python3.12 -m pip install -r requirements.txt
-
-# Collect static files
-python3.12 manage.py collectstatic --noinput --clear
+# Collect static files (ensure correct directory exists)
+python3 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
